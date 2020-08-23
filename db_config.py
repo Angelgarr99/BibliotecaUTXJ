063adc1190e7d62ac760b78a2af0f59d5023a37e
Angelgarr99 <63882563+Angelgarr99@users.noreply.github.com>
2020-08-23 10:14:45 -0500

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -4,8 +4,8 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
  
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = '1342'
+app.config['MYSQL_DATABASE_USER'] = 'AngelGarrido'
+app.config['MYSQL_DATABASE_PASSWORD'] = '1342.Adfs'
 app.config['MYSQL_DATABASE_DB'] = 'db_biblioteca'
-app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
+app.config['MYSQL_DATABASE_HOST'] = 'AngelGarrido.mysql.pythonanywhere-services.com'
 mysql.init_app(app)
